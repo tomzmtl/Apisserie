4591fea48a399c465bd494f26cac192b26df25e1
Thomas Andreo <thomas.andreo@gmail.com>
2018-05-05 18:16:19 -0400

    Disabled translations reducer

--- src/app/redux/reducers/index.js
+++ src/app/redux/reducers/index.js
@@ -1,6 +1,6 @@
 import { reducer as routerReducer } from 'fans-router';
 import products from './products';
-import translations from './translations';
+// import translations from './translations';
 import ui from './ui';
 import user from './user';
 
@@ -8,7 +8,7 @@ import user from './user';
 export default (state, action) => ({
   products: products(state.products, action),
   router: routerReducer(state.router, action, state),
-  translations: translations(state.translations, action, state),
+  // translations: translations(state.translations, action, state),
   ui: ui(state.ui, action, state),
   user: user(state.user, action, state),
 });
