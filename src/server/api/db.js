8ec4059f5790ea9e2ed2993a5763cf1e9f217747
Thomas Andreo <thomas.andreo@gmail.com>
2020-06-03 18:56:41 -0400

    Updated DB location

--- src/server/api/db.js
+++ src/server/api/db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 
 module.exports = () => {
-  mongoose.connect('mongodb://heroku_47z2274v:nrvuu3ri6g249m3s2kgfb4964b@ds139801.mlab.com:39801/heroku_47z2274v');
+  mongoose.connect('mongodb://heroku_87c15819:j1g6f6qspch165ntp2pkn2686v@ds227146.mlab.com:27146/heroku_87c15819');
   mongoose.Promise = global.Promise;
 };
